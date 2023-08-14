ba03fd1afd3414452336758f016b990846b209d1
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-14 12:32:44 +0600

    routes added

---
+++ routes/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const routes = express.Router()
+
+routes.get('/',(req,res) =>{
+    
+})
+
+routes.post('/',(req,res) =>{
+
+})
+
+routes.post('/all',(req,res) =>{
+
+})
+
+routes.put('/:id',(req,res) =>{
+
+})
+
+routes.delete('/:id',(req,res) =>{
+
+})
+
+
+module.exports=routes;
