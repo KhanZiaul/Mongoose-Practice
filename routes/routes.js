e5909432851d35e7ecf8f1520ef06b65976f0c1d
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-14 23:13:53 +0600

    put method done

--- routes/routes.js
+++ routes/routes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = express.Router()
 const todoSchema = require('../models/models')
-const Todo = new mongoose.model("Todo",todoSchema)
+const Todo = new mongoose.model("Todo", todoSchema)
 
-routes.get('/',(req,res) =>{
-    
+routes.get('/', (req, res) => {
+
 })
 
-routes.get('/:id',(req,res) =>{
-    
+routes.get('/:id', (req, res) => {
+
 })
 
 routes.post('/', async (req, res) => {
@@ -22,24 +22,48 @@ routes.post('/', async (req, res) => {
     }
 });
 
-routes.post('/all',async(req,res) =>{
+routes.post('/all', async (req, res) => {
 
-    try{
+    try {
         await Todo.insertMany(req.body)
         res.status(201).json({ message: "Successfully added new todo to the database" });
     }
-    catch(err){
+    catch (err) {
         res.status(400).json({ error: "Error occurred on the server side" });
     }
 })
 
-routes.put('/:id',(req,res) =>{
+// routes.put('/:id', async (req, res) => {
+//     try {
+//         await Todo.updateOne({ _id: req.params.id }, {
+//             $set: {
+//                 status: "inactive"
+//             }
+//         })
+//         res.status(201).json({ message: "Successfully added new todo to the database" });
+//     }
+//     catch (err) {
+//         res.status(400).json({ error: "Error occurred on the server side" });
+//     }
+// })
+
+routes.put('/:id', async (req, res) => {
+    try {
+        await Todo.updateOne({ _id: req.params.id }, {
+            $set: {
+                status: "inactive"
+            }
+        })
+        res.status(201).json({ message: "Successfully added new todo to the database" });
+    }
+    catch (err) {
+        res.status(400).json({ error: "Error occurred on the server side" });
+    }
+})
+
+routes.delete('/:id', (req, res) => {
 
 })
 
-routes.delete('/:id',(req,res) =>{
 
-})
-
-
-module.exports=routes;
+module.exports = routes;
