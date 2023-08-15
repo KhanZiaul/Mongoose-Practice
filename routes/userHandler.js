2683832e244030ac2c0fab06109dbdcf6c8ab351
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-15 20:35:49 +0600

    Login updated

--- routes/userHandler.js
+++ routes/userHandler.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
 const routes = express.Router()
 const userSchema = require('../models/userSchema')
 const User = new mongoose.model("User", userSchema)
 
+//  Signup
+
 routes.post('/signup', async (req, res) => {
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password,10);
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
         const newUser = new User({
-            name:req.body.name,
-            username:req.body.username,
-            password:hashedPassword
+            name: req.body.name,
+            username: req.body.username,
+            password: hashedPassword
         })
         await newUser.save()
-        res.status(200).json({ message: "User successfully added"});
+        res.status(200).json({ message: "User successfully added" });
     }
     catch (err) {
         res.status(400).json({ error: "Error occurred on the server side" });
@@ -22,5 +25,27 @@ routes.post('/signup', async (req, res) => {
 
 })
 
+// Login
+
+routes.post('/login', async (req, res) => {
+    try {
+        const user = await User.find({ username: req.body.username })
+        if (user && user.length > 0) {
+            const isValidPassword = await bcrypt.compare(req.body.password,user[0].password)
+            if (isValidPassword) {
+                
+            }
+            else {
+                res.status(401).json({ error: "Authentication error" });
+            }
+        } else {
+            res.status(401).json({ error: "Authentication error" });
+        }
+    }
+    catch (err) {
+        res.status(401).json({ error: "Authentication error" });
+    }
+
+})
 
 module.exports = routes;
