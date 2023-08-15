d6f6335f0b71d8175ef434a0a281c50f57703759
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-15 21:41:56 +0600

    checkLogin done

--- middlewares/checkLogin.js
+++ middlewares/checkLogin.js
@@ -1,11 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 const checkLogin = (req,res,next)=>{
+    const {authorization} = req.headers
     try{
-        
+        const token = authorization.split(' ')[1]
+        const decode = jwt.verify(token,process.env.JWT)
+        const {user , id} = decode
+        req.id=id
+        req.user= user
+        next()
     }
     catch(err){
-        res.status(401).json({ error: "Authentication error" });
+        next("Authentication error");
     }
 }
 
